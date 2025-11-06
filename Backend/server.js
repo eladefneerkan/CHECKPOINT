f999c84857f5d8a270837ed792f4e341947fa32c
eladefneerkan <edefneerkan@g.ucla.edu>
2025-11-05 21:25:30 -0800

    Buggy Commit on Purpose! Added Cors to server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const usersRoute = require("./routes/users");
+const cors = require("cors");
 
 dotenv.config();
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.use("/users", usersRoute);
 
--- src/App.jsx
+++ src/App.jsx
@@ -16,3 +16,31 @@ export default function App() {
     </Routes>
   );
 }
+
+
+/* -------------------------------------------------------------------------------- */
+import { useEffect, useState } from "react";
+
+function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/api/users")
+      .then((res) => res.json())
+      .then((data) => setUsers(data))
+      .catch((err) => console.error("Error:", err));
+  }, []);
+
+  return (
+    <div>
+      <h1>Users</h1>
+      <ul>
+        {users.map((u) => (
+          <li key={u.id}>{u.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
