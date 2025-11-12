bdf0319d878d842dd14859ac35b654721812406a
Jeffrey <jjeffrey0022@gmail.com>
2025-11-11 22:54:05 -0800

    Refactor gamedb.js, replace old API with new axios, removed unused files

--- Backend/routes/gamedb.js
+++ Backend/routes/gamedb.js
@@ -1,59 +1,53 @@
-const axios = require("axios");
-const express = require("express")
+import mongoose from "mongoose"
+import axios from "axios"
+import dotenv from "dotenv"
 
-const router = express.Router();
+const uri = process.env.MONGODB_URI || "mongodb://localhost:27017/checkpoint_db";
 
-const BASE_URL = "https://api.thegamesdb.net/v1";
-const API_KEY = process.env.API_KEY
-
-/**
- * @swagger
- * /games/search:
- *   get:
- *     summary: Search games by name
- *     parameters:
- *       - in: query
- *         name: name
- *         required: true
- *         schema:
- *           type: string
- *         description: Game name to search
- *     responses:
- *       200:
- *         description: List of games
- *         content:
- *           application/json:
- *             schema:
- *               type: array
- *               items:
- *                 type: object
- *                 properties:
- *                   game_id:
- *                     type: string
- *                   title:
- *                     type: string
- *                   platform:
- *                     type: string
- *                   description:
- *                     type: string
- */
-router.get("/search", async (req, res) => {
-    const { name } = req.query;
-    if (!name) return res.json([]);
-  try {
-        const response = await axios.get(`${BASE_URL}/Games/ByGameName`, {
-        params: { name }, // only the game name
-        headers: { Authorization: API_KEY }
-    });
-    const games = response.data?.data?.games;
-    res.json(games ? Object.values(games) : []);
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ error: "Failed to fetch from TheGamesDB" });
-  }
+mongoose.connect(uri)
+.then(() => console.log("mdb connected"))
+.catch((err) => {
+  console.error("mdb connection error:", err);
+  process.exit(1);
 });
 
 
-module.exports = router;
+const gameSchema = new mongoose.Schema({
+  id: Number,
+  name: String,
+  slug: String,
+  games_counts: Number,
+});
+
+const games = mongoose.model("Game", gameSchema);
+const apiKey = process.env.API_KEY
+
+const url = `https://api.rawg.io/api/games?key=${apiKey}&page_size=5`;
+
+try {
+  const response = await axios.get(url);
+  const games = response.data.results;
+
+  // Insert or update games in MongoDB
+  for (const g of games) {
+    await Game.updateOne(
+      { id: g.id },
+      {
+        name: g.name,
+        released: g.released,
+        rating: g.rating,
+        id: g.id,
+      },
+      { upsert: true }
+    );
+  }
+
+  console.log("🎮 Games added/updated successfully!");
+} catch (error) {
+  console.error("❌ Error fetching or saving data:", error.message);
+}
+
+mongoose.connection.close();
+
 
 
