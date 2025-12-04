d6cd74368ad097d218043612454c2af1f190ba1c
agsiddiqui0 <anamgsiddiqui@gmail.com>
2025-12-04 10:54:33 -0800

    refactor: removed unecessary import
    previous refactors allowed for the removal of the GameObj.js import, which now abides by the information hiding principle.

--- Frontend/GamePage.jsx
+++ Frontend/GamePage.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import {useLocation, useNavigate} from 'react-router-dom';
-import GameObj from './models/GameObj.js';
 import {parseDate, formatGenres, hydrateGame, getFullDescription} from './services/gameUtil';
 
 const GamePage = () => {
