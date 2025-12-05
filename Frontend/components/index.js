cb968fb1c612f54bf1fdc87f51644dd85c724caa
Scotoplane <scotoplane@g.ucla.edu>
2025-12-04 23:42:20 -0800

    Refactored home into components that we can reuse: ads, poster card and list, potion bubble effect

--- Frontend/components/index.js
+++ Frontend/components/index.js
@@ -1,7 +1,12 @@
 //entrypoint for all components in this folder for easier imports elsewhere
 import PotionBubbles from './globalUI/atoms/potionBubbles.jsx'; 
-
+import PosterCard from './globalUI/atoms/PosterCard.jsx'; 
+import PosterList from './globalUI/molecules/PosterList.jsx'; 
+import Advertisement from './globalUI/molecules/Advertisement.jsx';
 // Then, use named exports to make them available
 export { 
-  PotionBubbles
+    Advertisement,
+    PotionBubbles,
+    PosterCard,
+    PosterList
 };
