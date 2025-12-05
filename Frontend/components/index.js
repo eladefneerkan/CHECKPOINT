70af3e97c6783106c0e3fc07b07be97f4523747b
Scotoplane <scotoplane@g.ucla.edu>
2025-12-05 01:14:37 -0800

    Refactored more components into the component folder: logout button, page transition,

--- Frontend/components/index.js
+++ Frontend/components/index.js
@@ -3,10 +3,16 @@ import PotionBubbles from './globalUI/atoms/potionBubbles.jsx';
 import PosterCard from './globalUI/atoms/PosterCard.jsx'; 
 import PosterList from './globalUI/molecules/PosterList.jsx'; 
 import Advertisement from './globalUI/molecules/Advertisement.jsx';
+import LogoutButton from './globalUI/atoms/LogoutButton.jsx';
+import GameComp from './globalUI/molecules/GameComp.jsx';
+import PageTransition from './globalUI/atoms/PageTransition.jsx';
 // Then, use named exports to make them available
 export { 
     Advertisement,
     PotionBubbles,
     PosterCard,
-    PosterList
+    PosterList,
+    LogoutButton,
+    GameComp,
+    PageTransition
 };
