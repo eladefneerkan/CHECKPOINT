ad38da0fff4fd17384d98e7865e75ded73fed71e
Scotoplane <scotoplane@g.ucla.edu>
2025-12-04 21:55:47 -0800

    Refactoring the frontend to modularize components and clean up frontend

---
+++ Frontend/components/globalUI/atoms/potionBubbles.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+export default function PotionBubbles() {
+  const bubbles = Array.from({ length: 10 }, (_, i) => 10 + Math.random() * 10);
+
+  return (
+    <div className="potion-background">
+      {bubbles.map((_, i) => (
+        <div
+
+          key={i}
+          className="bubble"
+          style={{
+            left: `${Math.random() * 100}%`,
+            width: `${bubbles[i]}px`,
+            height: `${bubbles[i]}px`,
+            animationDuration: `${10 + Math.random() *15}s`,
+            animationDelay: `${Math.random() * 3}s`,
+          }}
+        ></div>
+      ))}
+    </div>
+  );
+}
+
---
+++ Frontend/components/index.js
@@ -0,0 +1,7 @@
+//entrypoint for all components in this folder for easier imports elsewhere
+import PotionBubbles from './globalUI/atoms/potionBubbles.jsx'; 
+
+// Then, use named exports to make them available
+export { 
+  PotionBubbles
+};
